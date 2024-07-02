818c1ca60f681ddfe638c5bc2fa72c229a87a9ea
ivantarabanovych <vanjokcitet334@gmail.com>
2024-07-03 00:36:16 +0300

    q1

---
+++ main.js
@@ -0,0 +1,6 @@
+for (var i = 1; i<=5; i++){
+    for (let j = 1; j <= 5; j++){ 
+        console.log(i + " x " + j + " = " + i*j);
+        document.write(i + " x " + j + " = " + i*j + "<br>")
+  }
+}
